bffdb9ed533ad3c8366c6cfecb2f02b808eb2800
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-05 22:58:21 -0500

    Created variables for container, row, column, and hide. Also created attributes for the variables. Finally appended them.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $("#start").on("click", function(){
     console.log("Hello World");
+
+
+    // Variables
+    var container = $(".container");
+    var row = $("<div>");
+    var column = $("<div>");
+    var hide = $("#hidden");
+
+    // Attributes
+    hide.attr("style", "display: none");
+    row.addClass("row");
+    column.addClass("col").attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
+
+
+    // Append
+    container.append(row, column);
+
+   
 })
