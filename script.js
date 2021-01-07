11d43087874223bce4113b3ef9d1a027087aef62
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-06 22:36:52 -0500

    Added an h3 for the question and buttons for the choices in the three stages of DOM Manipulation

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-$("#start").on("click", function(){
-    console.log("Hello World");
+$("#start").on("click", function () {
+  console.log("Hello World");
 
+  // Variables
+  var container = $(".container");
+  var row = $("<div>");
+  var column = $("<div>");
+  var hide = $("#hidden");
+  var question = $("<h3>");
+  var choicesOne = $("<button>");
+  var choicesTwo = $("<button>");
+  var choicesThree = $("<button>");
+  var choicesFour = $("<button>");
 
-    // Variables
-    var container = $(".container");
-    var row = $("<div>");
-    var column = $("<div>");
-    var hide = $("#hidden");
+  // Attributes
+  hide.attr("style", "display: none");
+  row.addClass("row");
+  column
+    .addClass("col")
+    .attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
+  question.text("Commonly used data types DO NOT Include:");
+  choicesOne.text("1. Quotes");
+  choicesTwo.text("2. Curly Brackets");
+  choicesThree.text("3. Parenthesis");
+  choicesFour.text("4. Square Brackets");
 
-    // Attributes
-    hide.attr("style", "display: none");
-    row.addClass("row");
-    column.addClass("col").attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
-
-
-    // Append
-    container.append(row, column);
-
-   
-})
+  // Append
+  column.append(question, choicesOne, choicesTwo, choicesThree, choicesFour);
+  container.append(row, column);
+});
