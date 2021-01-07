da01ed4234671480967f4ea025018c775bb2344f
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-06 23:49:28 -0500

    Created function for the if statement to be in. Put .empty() in the function newPage() to wipe away everything in the column.

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
+// Variables
+var row = $("<div>");
+var column = $("<div>");
+var question = $("<h3>");
+var choicesOne = $("<button>");
+var choicesTwo = $("<button>");
+var choicesThree = $("<button>");
+var choicesFour = $("<button>");
+var container = $(".container");
+var hide = $("#hidden");
 var count = 0;
 var timeLoss = 100;
+
+row.addClass("row");
+column
+  .addClass("col")
+  .attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
 
 $("#start").on("click", function () {
   console.log("Hello World");
 
-  // Variables
-  var container = $(".container");
-  var row = $("<div>");
-  var column = $("<div>");
-  var hide = $("#hidden");
-  var question = $("<h3>");
-  var choicesOne = $("<button>");
-  var choicesTwo = $("<button>");
-  var choicesThree = $("<button>");
-  var choicesFour = $("<button>");
-
   // Attributes
   hide.attr("style", "display: none");
-  row.addClass("row");
-  column
-    .addClass("col")
-    .attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
+
   question.text("Commonly used data types DO NOT Include:");
   choicesOne.text("1. Quotes").addClass("choicebtn");
   choicesTwo.text("2. Curly Brackets").addClass("choicebtn");
@@ -42,6 +43,7 @@ $("#start").on("click", function () {
       column.append(ans);
       count++;
       console.log(count);
+      newPage();
     } else {
       console.log("You are wrong pony boy");
 
@@ -49,6 +51,7 @@ $("#start").on("click", function () {
       timeLoss -= 10;
       console.log(timeLoss);
       column.append(ans);
+      newPage();
     }
     //   cities = renderNow(city);
   });
@@ -57,23 +60,8 @@ $("#start").on("click", function () {
 function newPage() {
   console.log("A Whole New World!!");
 
-  // Variables
-  var container = $(".container");
-  var row = $("<div>");
-  var column = $("<div>");
-  var hide = $("#hidden");
-  var question = $("<h3>");
-  var choicesOne = $("<button>");
-  var choicesTwo = $("<button>");
-  var choicesThree = $("<button>");
-  var choicesFour = $("<button>");
-
   // Attributes
-  hide.attr("style", "display: none");
-  row.addClass("row");
-  column
-    .addClass("col")
-    .attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
+  column.empty();
   question.text("Commonly used data types DO NOT Include:");
   choicesOne.text("1. Quotes").addClass("choicebtn");
   choicesTwo.text("2. Curly Brackets").addClass("choicebtn");
@@ -87,22 +75,27 @@ function newPage() {
   $(".choicebtn").on("click", function () {
     // var city = this.innerHTML;
     //   var city = $(this).data("city");
+    var clickOn = $(this).text();
     var hr = $("<hr>");
-    console.log("You click here!!");
-    if ($(this).text() === "1. Quotes") {
-      console.log("You're right!!");
-      var ans = $("<p>").text("Correct!");
-      column.append(ans);
-      count++;
-      console.log(count);
-    } else {
-      console.log("You are wrong pony boy");
-
-      var ans = $("<p>").text("Wrong!");
-      timeLoss -= 10;
-      console.log(timeLoss);
-      column.append(ans);
-    }
+    // console.log("You click here!!");
+    checkAns(clickOn);
     //   cities = renderNow(city);
   });
+}
+
+function checkAns(item) {
+  if (item === "1. Quotes") {
+    console.log("Item cor");
+    // var ans = $("<p>").text("Correct!");
+    // column.append(ans);
+    // count++;
+    // console.log(count);
+  } else {
+    console.log("item wron");
+
+    // var ans = $("<p>").text("Wrong!");
+    // timeLoss -= 10;
+    // console.log(timeLoss);
+    // column.append(ans);
+  }
 }
