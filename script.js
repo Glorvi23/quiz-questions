420c26100ff1be692459956903e55dcdcf4273b5
Caleb Glorvigen <jcglorvi@gmail.com>
2021-01-06 23:26:47 -0500

    Added function for selecting the choice buttons. Created conditional if statement to check if the correct one was selected.

--- script.js
+++ script.js
@@ -19,12 +19,30 @@ $("#start").on("click", function () {
     .addClass("col")
     .attr("style", "text-align: left; padding: 0% 27.8% 0% 27.8%;");
   question.text("Commonly used data types DO NOT Include:");
-  choicesOne.text("1. Quotes");
-  choicesTwo.text("2. Curly Brackets");
-  choicesThree.text("3. Parenthesis");
-  choicesFour.text("4. Square Brackets");
+  choicesOne.text("1. Quotes").addClass("choicebtn");
+  choicesTwo.text("2. Curly Brackets").addClass("choicebtn");
+  choicesThree.text("3. Parenthesis").addClass("choicebtn");
+  choicesFour.text("4. Square Brackets").addClass("choicebtn");
 
   // Append
   column.append(question, choicesOne, choicesTwo, choicesThree, choicesFour);
   container.append(row, column);
+
+  $(".choicebtn").on("click", function () {
+    // var city = this.innerHTML;
+    //   var city = $(this).data("city");
+    var hr = $("<hr>");
+    console.log("You click here!!");
+    if($(this).text() === "1. Quotes"){
+        console.log("You're right!!")
+        var ans = $("<p>").text("Correct!");
+        count++;
+    }else{
+        console.log("You are wrong pony boy");
+        
+        $("<p>").text("Wrong!")
+        timeLoss=- 10;
+    }
+    //   cities = renderNow(city);
+  });
 });
